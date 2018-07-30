a07e82ea5185ad9d48075c7bfa22e764aa55bede
Wesley Yando Tantra <wesleyyando17@gmail.com>
2018-07-30 13:43:03 +0700

    Fixing repo

--- FrmAddTransaction.Designer.cs
+++ FrmAddTransaction.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmAddTransaction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FrmAddTransaction";
             this.Text = "FrmAddTransaction";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
